80e3281b3567c6fa5ccbbfb4681309adfb0095f3
pTpTk <zxu44@u.rochester.edu>
2022-04-22 17:22:24 -0400

    adding environment.sh

---
+++ environment.sh
@@ -0,0 +1,4 @@
+# /bin/sh
+export PATH=/p/cs458/zxu44/local/bin:/usr/local/cuda/bin/:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
+export CPATH=/usr/local/cuda/targets/x86_64-linux/include:$CPATH
