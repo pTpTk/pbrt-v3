b4f060a0fee1347aea8604fb743e929180af8135
Abrar Rahman Protyasha <aprotyas@u.rochester.edu>
2022-04-23 23:55:30 -0400

    Update environment initialization script
    The script has been updated to load the CUDA module before exporting
    CUDA to path, and to only include user-local paths.
    
    Signed-off-by: Abrar Rahman Protyasha <aprotyas@u.rochester.edu>

--- environment.sh
+++ environment.sh
@@ -1,4 +1,5 @@
 # /bin/sh
-export PATH=/p/cs458/zxu44/local/bin:/usr/local/cuda/bin/:$PATH
+module load cuda
+export PATH=/p/cs458/$(whoami)/local/bin:/usr/local/cuda/bin/:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
 export CPATH=/usr/local/cuda/targets/x86_64-linux/include:$CPATH
