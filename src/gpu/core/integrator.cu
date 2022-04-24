d73740714aadb1c86411428de7f91b5df62d6c2e
Abrar Rahman Protyasha <aprotyas@u.rochester.edu>
2022-04-24 01:53:08 -0400

    Use gpu::shared_ptr<T> in the Scene class
    Signed-off-by: Abrar Rahman Protyasha <aprotyas@u.rochester.edu>

--- src/gpu/core/integrator.cu
+++ src/gpu/core/integrator.cu
@@ -69,8 +69,8 @@ Spectrum UniformSampleOneLight(const Interaction &it, const Scene &scene,
         lightNum = min((int)(sampler.Get1D() * nLights), nLights - 1);
         lightPdf = Float(1) / nLights;
     }
-    const std::shared_ptr<Light> &light = scene.lights[lightNum];
     // TODO: Fix operator::[] call
+    const shared_ptr<Light> &light = scene.lights[lightNum];
     Point2f uLight = sampler.Get2D();
     Point2f uScattering = sampler.Get2D();
     // TODO: Fix pointer dereference call
