ece4f6f5d09926bb8ec3b90fc5b835b6dafa1aad
Abrar Rahman Protyasha <aprotyas@u.rochester.edu>
2022-04-24 00:00:35 -0400

    Add environment script usage instruction
    Signed-off-by: Abrar Rahman Protyasha <aprotyas@u.rochester.edu>

--- environment.sh
+++ environment.sh
@@ -1,4 +1,5 @@
 # /bin/sh
+# To use: `source environment.sh`
 module load cuda
 export PATH=/p/cs458/$(whoami)/local/bin:/usr/local/cuda/bin/:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
